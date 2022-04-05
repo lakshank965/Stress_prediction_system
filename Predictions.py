d981c7c820bb16b9c0420eeb89a815b6185ebf6e
KavinduLakshanWicramasinghe <lakshank965@gmail.com>
2022-04-06 00:28:09 +0530

    defining of make_predictions function. modify emotion_counts_find function.

--- Predictions.py
+++ Predictions.py
@@ -1,14 +1,42 @@
 from Operations import Database, Predictions, FaceEmotionTracking
 
 
-def count_emotion(date, emp_id, emotion):
+# ----------------------------------------------------------------------------------------------------------------------
+def emotion_counts_find(date, emp_id, emotion):
     db = Database('face-emotion-counting', 'eIWnBiQ1eIQuwO97')
     db.make_connection()
 
-    emotion_count = FaceEmotionTracking.objects(date=date, emp_id=emp_id, emotion=emotion).count()
-    print(emotion_count)
+    specific_emotion_count = FaceEmotionTracking.objects(date=date, emp_id=emp_id, emotion=emotion).count()
+    all_emotion_count = FaceEmotionTracking.objects(date=date, emp_id=emp_id).count()
+
+    # specific_emotion_percentage = (specific_emotion_count / all_emotion_count) * 100.0
 
     db.close_connection()
+    return specific_emotion_count, all_emotion_count
+# ----------------------------------------------------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------------------------------------------------
+def make_predictions(date, emp_id):
+    predictions = {date: date, emp_id: emp_id}
+    emo_counts = {}
+    emo_percentages = {}
 
-count_emotion('2022-03-14', 'emp003', 'Neutral')
+    emotions = ['Anger', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+    for emotion in emotions:
+        counts = emotion_counts_find(date, emp_id, emotion)
+        specific_emo_count = counts[0]
+        all_emo_count = counts[1]
+
+        emo_counts[emotion] = specific_emo_count
+
+        percentage = (specific_emo_count / all_emo_count) * 100.0
+        emo_percentages[emotion] = percentage
+
+    effective_emotions = emo_counts['Anger'] + emo_counts['Disgust'] + emo_counts['Fear'] + emo_counts['Sad']
+    stress_percentage = (effective_emotions / all_emo_count) * 100.0
+
+    predictions['emo_percentages'] = emo_percentages
+    predictions['stress_percentage'] = stress_percentage
+
+    return predictions
+# ----------------------------------------------------------------------------------------------------------------------
