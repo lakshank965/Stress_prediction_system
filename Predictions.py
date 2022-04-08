61684f837fa6b0fe76c5105fe9eca7782a09ad74
KavinduLakshanWicramasinghe <lakshank965@gmail.com>
2022-04-09 00:46:55 +0530

    ZeroDivisionError handle

--- Predictions.py
+++ Predictions.py
@@ -3,15 +3,15 @@ from Operations import Database, Predictions, DailyPredictions, FaceEmotionTrack
 
 # ----------------------------------------------------------------------------------------------------------------------
 def emotion_counts_find(date, emp_id, emotion):
-    db = Database('face-emotion-counting', 'eIWnBiQ1eIQuwO97')
-    db.make_connection()
+    # db = Database('face-emotion-counting', 'eIWnBiQ1eIQuwO97')
+    # db.make_connection()
 
     specific_emotion_count = FaceEmotionTracking.objects(date=date, emp_id=emp_id, emotion=emotion).count()
     all_emotion_count = FaceEmotionTracking.objects(date=date, emp_id=emp_id).count()
 
     # specific_emotion_percentage = (specific_emotion_count / all_emotion_count) * 100.0
 
-    db.close_connection()
+    # db.close_connection()
     return specific_emotion_count, all_emotion_count
 
 
@@ -32,11 +32,19 @@ def make_predictions(date, emp_id):
 
         emo_counts[emotion] = specific_emo_count
 
-        percentage = (specific_emo_count / all_emo_count) * 100.0
+        try:
+            percentage = (specific_emo_count / all_emo_count) * 100.0
+        except ZeroDivisionError:
+            percentage = 0.0
+
         emo_percentages[emotion] = percentage
 
     effective_emotions = emo_counts['Anger'] + emo_counts['Disgust'] + emo_counts['Fear'] + emo_counts['Sad']
-    stress_percentage = (effective_emotions / all_emo_count) * 100.0
+
+    try:
+        stress_percentage = (effective_emotions / all_emo_count) * 100.0
+    except ZeroDivisionError:
+        stress_percentage = 0.0
 
     predictions['emo_counts'] = emo_counts
     predictions['emo_percentages'] = emo_percentages
@@ -49,17 +57,18 @@ def make_predictions(date, emp_id):
 
 # ----------------------------------------------------------------------------------------------------------------------
 def write_predictions(predictions):
-    db = Database('predictions-write', 'yBAGoHEuXurabm63')
-    db.make_connection()
+    # db = Database('predictions-write', 'yBAGoHEuXurabm63')
+    # db.make_connection()
 
     data = Predictions.objects(date=predictions['date'], emp_id=predictions['emp_id']).first()
 
     if data:
-        data.update(emo_counts=predictions['emo_counts'], emo_percentages=predictions['emo_percentages'], stress_percentage=predictions['stress_percentage'])
-        db.close_connection()
+        data.update(emo_counts=predictions['emo_counts'], emo_percentages=predictions['emo_percentages'],
+                    stress_percentage=predictions['stress_percentage'])
+        # db.close_connection()
 
-        db = Database('daily-predictions', 'b1xvQn1CBeoBf2a6')
-        db.make_connection()
+        # db = Database('daily-predictions', 'b1xvQn1CBeoBf2a6')
+        # db.make_connection()
 
         daily_report = DailyPredictions.objects(date=predictions['date']).first()
         if daily_report:
@@ -73,12 +82,20 @@ def write_predictions(predictions):
                 all_count += specific_emo_count
 
             for emotion in daily_report.all_emo_percentages.keys():
-                all_emo_percentages[emotion] = (all_emo_counts[emotion] / all_count) * 100.0
+                try:
+                    all_emo_percentages[emotion] = (all_emo_counts[emotion] / all_count) * 100.0
+                except ZeroDivisionError:
+                    all_emo_percentages[emotion] = 0.0
 
-            all_effective_emotions = all_emo_counts['Anger'] + all_emo_counts['Disgust'] + all_emo_counts['Fear'] + all_emo_counts['Sad']
-            average_stress_percentage = (all_effective_emotions / all_count) * 100.0
+            all_effective_emotions = all_emo_counts['Anger'] + all_emo_counts['Disgust'] + all_emo_counts['Fear'] + \
+                                     all_emo_counts['Sad']
+            try:
+                average_stress_percentage = (all_effective_emotions / all_count) * 100.0
+            except ZeroDivisionError:
+                average_stress_percentage = 0.0
 
-            daily_report.update(all_emo_counts=all_emo_counts, all_emo_percentages=all_emo_percentages, average_stress_percentage=average_stress_percentage)
+            daily_report.update(all_emo_counts=all_emo_counts, all_emo_percentages=all_emo_percentages,
+                                average_stress_percentage=average_stress_percentage)
 
         else:
             emotions = ['Anger', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
@@ -92,10 +109,18 @@ def write_predictions(predictions):
                 all_count += specific_emo_count
 
             for emotion in emotions:
-                all_emo_percentages[emotion] = (all_emo_counts[emotion] / all_count) * 100.0
+                try:
+                    all_emo_percentages[emotion] = (all_emo_counts[emotion] / all_count) * 100.0
+                except ZeroDivisionError:
+                    all_emo_percentages[emotion] = 0.0
 
-            all_effective_emotions = all_emo_counts['Anger'] + all_emo_counts['Disgust'] + all_emo_counts['Fear'] + all_emo_counts['Sad']
-            average_stress_percentage = (all_effective_emotions / all_count) * 100.0
+            all_effective_emotions = all_emo_counts['Anger'] + all_emo_counts['Disgust'] + all_emo_counts['Fear'] + \
+                                     all_emo_counts['Sad']
+
+            try:
+                average_stress_percentage = (all_effective_emotions / all_count) * 100.0
+            except ZeroDivisionError:
+                average_stress_percentage = 0.0
 
             daily_pred = DailyPredictions()
             daily_pred.date = predictions['date']
@@ -104,11 +129,11 @@ def write_predictions(predictions):
             daily_pred.average_stress_percentage = average_stress_percentage
             daily_pred.save()
 
-        db.close_connection()
+        # db.close_connection()
 
     else:
-        db = Database('predictions-write', 'yBAGoHEuXurabm63')
-        db.make_connection()
+        # db = Database('predictions-write', 'yBAGoHEuXurabm63')
+        # db.make_connection()
 
         pred = Predictions()
         pred.date = predictions['date']
@@ -118,10 +143,10 @@ def write_predictions(predictions):
         pred.stress_percentage = predictions['stress_percentage']
         pred.save()
 
-        db.close_connection()
+        # db.close_connection()
 
-        db = Database('daily-predictions', 'b1xvQn1CBeoBf2a6')
-        db.make_connection()
+        # db = Database('daily-predictions', 'b1xvQn1CBeoBf2a6')
+        # db.make_connection()
 
         daily_report = DailyPredictions.objects(date=predictions['date']).first()
         if daily_report:
@@ -135,12 +160,20 @@ def write_predictions(predictions):
                 all_count += specific_emo_count
 
             for emotion in daily_report.all_emo_percentages.keys():
-                all_emo_percentages[emotion] = (all_emo_counts[emotion] / all_count) * 100.0
+                try:
+                    all_emo_percentages[emotion] = (all_emo_counts[emotion] / all_count) * 100.0
+                except ZeroDivisionError:
+                    all_emo_percentages[emotion] = 0.0
 
-            all_effective_emotions = all_emo_counts['Anger'] + all_emo_counts['Disgust'] + all_emo_counts['Fear'] + all_emo_counts['Sad']
-            average_stress_percentage = (all_effective_emotions / all_count) * 100.0
+            all_effective_emotions = all_emo_counts['Anger'] + all_emo_counts['Disgust'] + all_emo_counts['Fear'] + \
+                                     all_emo_counts['Sad']
+            try:
+                average_stress_percentage = (all_effective_emotions / all_count) * 100.0
+            except ZeroDivisionError:
+                average_stress_percentage = 0.0
 
-            daily_report.update(all_emo_counts=all_emo_counts, all_emo_percentages=all_emo_percentages, average_stress_percentage=average_stress_percentage)
+            daily_report.update(all_emo_counts=all_emo_counts, all_emo_percentages=all_emo_percentages,
+                                average_stress_percentage=average_stress_percentage)
 
         else:
             emotions = ['Anger', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
@@ -154,10 +187,18 @@ def write_predictions(predictions):
                 all_count += specific_emo_count
 
             for emotion in emotions:
-                all_emo_percentages[emotion] = (all_emo_counts[emotion] / all_count) * 100.0
+                try:
+                    all_emo_percentages[emotion] = (all_emo_counts[emotion] / all_count) * 100.0
+                except ZeroDivisionError:
+                    all_emo_percentages[emotion] = 0.0
 
-            all_effective_emotions = all_emo_counts['Anger'] + all_emo_counts['Disgust'] + all_emo_counts['Fear'] + all_emo_counts['Sad']
-            average_stress_percentage = (all_effective_emotions / all_count) * 100.0
+            all_effective_emotions = all_emo_counts['Anger'] + all_emo_counts['Disgust'] + all_emo_counts['Fear'] + \
+                                     all_emo_counts['Sad']
+
+            try:
+                average_stress_percentage = (all_effective_emotions / all_count) * 100.0
+            except ZeroDivisionError:
+                average_stress_percentage = 0.0
 
             daily_pred = DailyPredictions()
             daily_pred.date = predictions['date']
@@ -166,7 +207,7 @@ def write_predictions(predictions):
             daily_pred.average_stress_percentage = average_stress_percentage
             daily_pred.save()
 
-        db.close_connection()
+        # db.close_connection()
 
     return 'predictions data updated.'
 # ----------------------------------------------------------------------------------------------------------------------
